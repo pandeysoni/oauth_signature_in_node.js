a7fda535362fbbf2135eccf09d6d63891dd37f05
soni pandey <sonipandey.71@gmail.com>
2020-04-16 15:27:07 +0530

    configured some parametes

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    consumer_key: 'soni_pandey',
+    secret_key: 'e7zjz7ZN2U4ZRhfV3WpwPA',
+    base_url: 'https://schoolwebsite.org/campus/oneroster/schoolName/learningdata/v1/schools',
+    method: 'GET',
+    queryParameters: {
+                        offset:0,
+                        limit:100,
+                        filter:"status='active'"
+    }
+}
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const method = 'GET';
-const base_url = 'https://schoolwebsite.org/campus/oneroster/schoolName/learningdata/v1/schools';
-const consumer_key = `e7zjz7ZN2U4ZRhfV3WpwPA`;
-
 const crypto = require('crypto');
+const uuid = require('uuid');
+const Config = require('./config');
+
+const method = Config.method;
+const base_url = Config.base_url;
+const secret_key = Config.secret_key;
+
+const oauth_timestamp = Math.floor(Date.now() / 1000);
+const oauth_nonce = uuid.v1();
+
 const parameters = {
-    offset:0,
-    limit:100,
-    filter:"status='active'",
-    oauth_consumer_key:"soni_pandey",
-    oauth_signature_method:"HMAC-SHA1",
-    oauth_timestamp:"1587025108",
-    oauth_nonce:"adf979a5b9e6",
-    oauth_version:"1.0"
+    ...Config.queryParameters,
+    oauth_consumer_key: Config.consumer_key,
+    oauth_signature_method: 'HMAC-SHA1',
+    oauth_timestamp: oauth_timestamp,
+    oauth_nonce: oauth_nonce,
+    oauth_version: '1.0'
 }
 let ordered = {};
 Object.keys(parameters).sort().forEach(function(key) {
@@ -37,12 +41,16 @@ const signature_base_string = `${method}&${encodedUrl}&${encodedParameters}`
 
 console.log(signature_base_string);
 
-const signing_key = `${consumer_key}&`; //as token is missing in our case.
+const signing_key = `${secret_key}&`; //as token is missing in our case.
 
-const oauth_signature = crypto.createHmac("sha1", signing_key).update(signature_base_string).digest().toString('base64');
+const oauth_signature = crypto.createHmac('sha1', signing_key).update(signature_base_string).digest().toString('base64');
 
 console.log(oauth_signature);
 
 const encoded_oauth_signature = encodeURIComponent(oauth_signature);
 
 console.log(encoded_oauth_signature);
+
+const authorization_header = `OAuth oauth_consumer_key="${Config.consumer_key}",oauth_signature_method="HMAC-SHA1",oauth_timestamp="${oauth_timestamp}",oauth_nonce="${oauth_nonce}",oauth_version="1.0",oauth_signature="${encoded_oauth_signature}"`
+
+console.log(authorization_header);
